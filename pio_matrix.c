4be74024cecdb7d8c71cb32059bbdcdd27804ccc
Claudio-Marinho <claudio_jr27@hotmail.com>
2025-01-25 11:25:03 -0300

    Update pio_matrix.c
    Add tecla 4

--- pio_matrix.c
+++ pio_matrix.c
@@ -4,35 +4,34 @@
 #include "hardware/clocks.h"
 #include "pico/bootrom.h"
 #include "pio_matrix.pio.h" 
-#include "init_GPIO.h"//biblioteca que inicializa teclado e o botao
-#include "frames.h"//biblioteca com os frames
-#include "config_leds.h"//biblioteca com a config dos leds
+#include "init_GPIO.h" // biblioteca que inicializa teclado e o botão
+#include "frames.h" // biblioteca com os frames
+#include "config_leds.h" // biblioteca com a config dos leds
 
-//Debounce (em milissegundos)
+// Debounce (em milissegundos)
 #define DEBOUNCE_TIME 300
 
-//função principal
+// Função principal
 int main()
 {
     // Inicializa todos os códigos stdio padrão que estão ligados ao binário.
     stdio_init_all();
-    //inicializa teclado e btn0, função no arquivo init_GPIO.h
+    // Inicializa teclado e btn0, função no arquivo init_GPIO.h
     init_teclado_btn0();
     
     PIO pio = pio0; 
     uint32_t valor_led;
-    double r = 0.0, b = 0.0 , g = 0.0; 
+    double r = 0.0, b = 0.0, g = 0.0; 
     bool ok;
-    ok = set_sys_clock_khz(128000, false);//coloca a frequência de clock para 128 MHz, facilitando a divisão pelo clock    
+    ok = set_sys_clock_khz(128000, false); // Coloca a frequência de clock para 128 MHz, facilitando a divisão pelo clock    
 
-    //configurações da PIO
+    // Configurações da PIO
     uint offset = pio_add_program(pio, &pio_matrix_program);
     uint sm = pio_claim_unused_sm(pio, true);
     pio_matrix_program_init(pio, sm, offset, OUT_PIN);
 
     while (true) 
     {
-        
         char tecla = ler_teclado();
         switch (tecla)
         {
@@ -52,35 +51,40 @@ int main()
             break;
 
         case '2':
-            
             break;
 
         case '3':
-            
             break;
 
         case '4':
-            
+            // Função para mostrar animação quando a tecla '4' for pressionada
+            setar_leds_azul(tecla4_frame1, valor_led, pio, sm, r, g, b);
+            sleep_ms(500);
+            setar_leds_azul(tecla4_frame2, valor_led, pio, sm, r, g, b);
+            sleep_ms(500);
+            setar_leds_verde(tecla4_frame3, valor_led, pio, sm, r, g, b);
+            sleep_ms(500);
+            setar_leds_vermelho(tecla4_frame4, valor_led, pio, sm, r, g, b);
+            sleep_ms(500);
+            setar_leds_verde(tecla4_frame5, valor_led, pio, sm, r, g, b);
+            sleep_ms(500);
+            setar_leds_azul(tecla4_frame6, valor_led, pio, sm, r, g, b);
+            sleep_ms(500);
             break;
 
         case '5':
-            
             break;
 
         case '6':
-            
             break;
 
         case '7':
-            
             break;
 
         case '8':
-            
             break;
 
         case '9':
-            
             break;
 
         case '0':
@@ -114,8 +118,8 @@ int main()
         default:
             break;
         }
-        //tempo de debounce para o botão
+        // Tempo de debounce para o botão
         sleep_ms(DEBOUNCE_TIME);
-        printf("\nfrequência de clock %ld\r\n", clock_get_hz(clk_sys));
+        printf("\nFrequência de clock %ld\r\n", clock_get_hz(clk_sys));
     }
 }
